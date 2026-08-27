bb0c9d4750241aebe79738a8e367a6c03950a76d
peeremerusia <tenpeerawattt@gmail.com>
2026-08-27 13:49:54 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded',function () {
+  document.getElementById('year').innerHTML = new Date().getFullYear();
+  console.log('Portfolio ของ PEERAMERUSIA พร้อมแล้ว!');
+});
